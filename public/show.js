7713fde35228436fe85cd44a800db22c4cf287ea
juanBerger <juan@duotoneaudio.com>
2021-11-21 00:36:00 -0500

    url

--- public/show.js
+++ public/show.js
@@ -5,7 +5,7 @@ var xhr = new XMLHttpRequest();
 let images = {}
 
 
-xhr.open("GET", "./public/img", true);
+xhr.open("GET", "/img", true);
 //xhr.open("GET", base + '/public/img', true);
 xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencode');
